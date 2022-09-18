18ef19cfd2361cf196d9735eea9c61a317c558b9
Romeo VINCENT <rvincent@e1r6p20.clusters.42paris.fr>
2022-09-18 21:03:45 +0200

    push

--- philo.c
+++ philo.c
@@ -1,29 +1,40 @@
-#include <stdio.h>
-#include <string.h>
-#include <pthread.h>
+#include "philo.h"
 
-int i = 2;
+// void	*function(void *p)
+// {
+// 	// Print value received as argument:
+// 	printf("Value recevied as argument in starting routine: ");
+// 	printf("%i\n", *(int *)p);
+// 	// Return reference to global variable:
+// }
 
-void* foo(void* p){
-  // Print value received as argument:
-  printf("Value recevied as argument in starting routine: ");
-  printf("%i\n", * (int*)p);
-
-  // Return reference to global variable:
-  pthread_exit(&i);
-}
-
-int main(void){
-  // Declare variable for thread's ID:
-  pthread_t id;
-
-  int j = 1;
-  pthread_create(&id, NULL, foo, &j);
-    
-  int* ptr;
-
-  // Wait for foo() and retrieve value in ptr;
-  pthread_join(id, (void**)&ptr);
-  printf("Value recevied by parent from child: ");
-  printf("%i\n", *ptr);
+int	main(int argc, char **argv)
+{
+	// pthread_t t[argc - 1];
+	// int i;
+	t_data data;
+	// if (argc != 5 && argc != 5)
+	// {
+	// 	printf("Wrong inputs\n");
+	// 	exit(1);
+	// }
+	data.number_of_philosophers = atoi(argv[1]);
+	// data.time_to_die = argv[2];
+	// data.time_to_eat = argv[3];
+	// data.time_to_sleep = argv[4];
+	// if (argv[5])
+	// 	data.number_of_times_each_philosopher_must_eat = argv[5];
+	// i = 1;
+	// while (argv[i])
+	// {
+	// 	int j = 10;
+	// 	pthread_create(t[i - 1], NULL, function, &data);
+	// 	pthread_join(t[i - 1], NULL);
+	// }
+	int *philos;
+	philos = malloc(data.number_of_philosophers * sizeof(int));
+	memset((void *)philos, 1, data.number_of_philosophers*sizeof(int));
+	// int i = data.number_of_philosophers;
+	// while (i)
+	printf("%d\n", philos[0]);
 }
--- philo.h
+++ philo.h
@@ -1,4 +1,19 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+typedef struct s_data
+{
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
+}		t_data;
+
+
 #endif
